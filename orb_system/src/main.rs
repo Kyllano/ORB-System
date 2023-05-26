ed005faf7dff928e5a48c5fe1a0e143f7442abca
Kyllano <kyll@tutanota.com>
2023-05-26 09:34:02 +0000

    On change, je fias du faux objet (je pense que ca marchera mieu)

--- orb_system/src/main.rs
+++ orb_system/src/main.rs
@@ -1,31 +1,80 @@
 use std::fs;
 
+/*
 #[derive(Debug)]
-struct File <'a>{
-    filename: &'a str,
+enum Node<'a> {
+    File(&'a str),
+    Directory(&'a str, Vec<Node<'a>>),
 }
 
-#[derive(Debug)]
-enum Node {
-    File(File),
-    Directory(String, Vec<Node>),
-}
-
-impl Node {
-    fn new_file(filename: &str) -> Node {
-        Node::File(File { filename })
+impl<'a> Node <'a> {
+    fn new_file(filename: &'a str) -> Self {
+        Node::File(filename)
     }
+    
+    fn new_empty_directory(directoryname :&'a str, children: Vec<Node<'a>>) -> Self {
+        Node::Directory(directoryname, children)
+    }
+    
+    fn get_children(&self) -> &Vec<Node<'a>> {
+        match self {
+            Node::Directory(_,_) => {
+                self.children
+            }
+            Node::File(_,_) => {
+                None
+            }
+        }
+    }
+}
+*/
+enum FileSystem <'a> {
+    Directory(Directory<'a>),
+    File(File<'a>)
+}
 
-    fn new_directory(name: String, children: Vec<Node>) -> Node {
-        Node::Directory(name, children)
+struct Directory <'a>{
+    name : &'a str,
+    children : Vec<FileSystem<'a>>
+}
+
+struct File <'a> {
+    name : &'a str
+}
+
+trait Name {
+    fn name(&self) -> &str;
+}
+
+trait Parent {
+    fn get_children(&self) -> &Vec<FileSystem>;
+}
+
+impl<'a> Name for Directory<'a> {
+    fn name(&self) -> &str {
+        self.name
+    }
+}
+
+impl<'a> Name for File<'a> {
+    fn name(&self) -> &str {
+        self.name
+    }
+}
+
+impl<'a> Parent for Directory<'a> {
+    fn get_children(&self) -> &Vec<FileSystem<'a>> {
+        &self.children
     }
 }
 
 
 fn main() {
+    /*
     let data = fs::read("../README.md").expect("Unable to open file");
     println!("{}", data.len());
     let data = String::from_utf8(data).expect("Couldn't parse UTF 8");
-
     println!("{data}");
+    */
+    
 }
