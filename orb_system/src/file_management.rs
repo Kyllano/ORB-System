9c1c24e4c4f2e4701c12f8556cdd4772dbe9298a
Kyllano <kyll@tutanota.com>
2023-05-31 20:55:53 +0000

    serialization et deserialization des dossier & fichier finito

--- orb_system/src/file_management.rs
+++ orb_system/src/file_management.rs
@@ -1,6 +1,5 @@
 
-use std::fs::{self, FileType};
-
+use std::{fs::{self, FileType}, error::Error};
 
 
 pub struct File {
@@ -62,22 +61,24 @@ impl File{
         //On ajoute ensuite la data au fichier
         binary_file.extend(&self.data);
         self.data.clear(); //Pour nettoyer un peu la mémoire et pouvoir gérer plusieurs fichiers a peu près gros (ne dépassant pas la RAM)
-
-        //println!("{:?}", binary_file);
         binary_file
     }
 
-    pub fn deserialize(binary : Vec<u8>) -> File {
-        let mut offset : usize = 0;
-        //TODO : On verifiera la taille du fichier plus tard ...
+    pub fn deserialize(binary : &Vec<u8>) -> Result<File, &'static str> {
+        Self::deserialize_f(binary, 0)
+    }
+
+    fn deserialize_f(binary : &Vec<u8>, offset_f : usize) -> Result<File, &'static str> {
+        let mut offset : usize = offset_f;
 
         //On récupère la taille du path
-        let mut len_path_byte : [u8;8] = [0;8];
-        len_path_byte.copy_from_slice(&binary[0..8]);
-        let len_path : usize = u64::from_le_bytes(len_path_byte) as usize;
+        let len_path : usize = deserialize_u64(&binary, &offset)? as usize;
         offset += 8;
 
         //On récupère le path
+        if binary.len() < offset + len_path{
+            return Err("Path sized written in binary is incorrect");
+        }
         let mut path_bytes : Vec<u8> = Vec::new();
         for i in offset..offset + len_path {
             path_bytes.push(binary[i]);
@@ -86,18 +87,19 @@ impl File{
         offset += len_path;
 
         //On récupère la longueur de la data
-        let mut len_data_byte : [u8;8] = [0;8];
-        len_data_byte.copy_from_slice(&binary[offset..offset+8]);
-        let len_data : usize = u64::from_le_bytes(len_data_byte) as usize;
+        let len_data : usize = deserialize_u64(&binary, &offset)? as usize;
         offset += 8;
 
         //On récupère la data
+        if binary.len() < offset + len_data{
+            return Err("File size written in binary is incorrect");
+        }
         let mut data : Vec<u8> = Vec::new();
         for i in offset..offset + len_data {
             data.push(binary[i]);
         }
 
-        File::new(&path, data)
+        Ok(Self::new(&path, data))
     }
 }
 
@@ -168,6 +170,107 @@ impl Directory{
         }
     }
 
+    pub fn serialize(&mut self) -> Vec<u8> {
+        //On créé le fichier binaire
+        let mut binary_file : Vec<u8> = Vec::new();
+
+        //On ajoute la taille du path au binary
+        let len_path : u64 = self.path.len() as u64;
+        let len_path : [u8;8] = len_path.to_le_bytes();
+        binary_file.extend(len_path); //len_path est moved du coup deallocated
+
+        //On ajoute le path au fichier
+        binary_file.extend(self.path.as_bytes());
+
+        //On ajoute ensuite le nombre de fichier dans le binary
+        let nb_file : u64 = self.children_file.len() as u64;
+        let nb_file : [u8;8] = nb_file.to_le_bytes();
+        binary_file.extend(nb_file);
+
+        //On ajoute ensuite chaque fichier serializé au binary
+        for file in &mut self.children_file {
+            binary_file.extend(file.serialize());
+        }
+
+        //On ajoute ensuite le nombre de dossier dans le binary
+        let nb_dir : u64 = self.children_directory.len() as u64;
+        let nb_dir : [u8;8] = nb_dir.to_le_bytes();
+        binary_file.extend(nb_dir);
+
+        //On serialize recursivement chaque dossier
+        for dir in &mut self.children_directory {
+            binary_file.extend(dir.serialize());
+        }
+
+        binary_file
+    }
+
+    pub fn deserialize(binary : &Vec<u8>) -> Result<Directory, &'static str>{
+        Directory::deserialize_d(binary, 0)
+    }
+
+    fn deserialize_d(binary : &Vec<u8>, offset_f : usize) -> Result<Directory, &'static str>{
+        let mut offset : usize = offset_f;
+
+        //On récupère la taille du path
+        let len_path : usize = deserialize_u64(&binary, &offset)? as usize;
+        offset += 8;
+
+        //On récupère le path
+        if binary.len() < offset + len_path{
+            return Err("Path sized written in binary is incorrect");
+        }
+        let mut path_bytes : Vec<u8> = Vec::new();
+        for i in offset..offset + len_path {
+            path_bytes.push(binary[i]);
+        }
+        let path : String = String::from_utf8(path_bytes).expect("Invalid UTF-8 value");
+        offset += len_path;
+
+        //On créé l'objet Directory
+        let mut dir = Self::new_empty(&path);
+
+        //On lit le nombre de fichier présents
+        let nb_files : usize = deserialize_u64(&binary, &offset)? as usize;
+        offset += 8;
+
+        //Pour chaque fichier, on le deserialize et on l'ajoute au dossier
+        for _ in 0..nb_files{
+            let file : File = File::deserialize_f(&binary, offset)?;
+            offset += 8 + file.path.len() + 8 + file.data.len();
+            dir.children_file.push(file);
+        }
+
+        //On lit le nombre de dossier présent
+        let nb_dir : usize = deserialize_u64(&binary, &offset)? as usize;
+        offset += 8;
+
+        //Pour chaque dossier, on le deserialize et on l'ajoute au dossier
+        for _ in 0..nb_dir{
+            let sub_dir : Directory = Self::deserialize_d(&binary, offset)?;
+            offset += Self::get_offset_dir_deserialization(&sub_dir);
+            dir.children_directory.push(sub_dir);
+        }
+
+        Ok(dir)
+    }
+
+    pub fn get_offset_dir_deserialization(dir : &Directory) -> usize {
+        let mut offset : usize = 0;
+        offset += 8 + dir.path.len();   //Pour trouver le titre
+        offset += 8;                    //Pour le nombre de fichier
+        for file in &dir.children_file{
+            //Pour la taille du path + le path et pour la taille de la data + la data
+            offset += 8 + file.path.len() + 8 + file.data.len();
+        }
+        offset += 8;                    //Pour le nombre de dossier
+        for dir in &dir.children_directory{
+            offset += Self::get_offset_dir_deserialization(&dir);
+        }
+
+        offset
+    }
+
     pub fn print_dir(&self, indent_lvl : usize){
         let path_split: Vec<&str> = self.path.split('/').collect();
         let name = path_split[path_split.len()-1];
@@ -194,4 +297,13 @@ impl Directory{
             println!("{}└ {}", "  ".repeat(indent_lvl+1), child.get_name());
         }
     }
+}
+
+fn deserialize_u64(binary : &Vec<u8>, offset : &usize) -> Result<u64, &'static str> {
+    if binary.len() < offset+8{
+        return Err("binary is too small to have a u64 at this offset location");
+    }
+    let mut num : [u8;8] = [0;8];
+    num.copy_from_slice(&binary[*offset..*offset+8]);
+    Ok(u64::from_le_bytes(num))
 }
--- orb_system/src/main.rs
+++ orb_system/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code,unused_imports)]
+
 mod file_management;
 use file_management::Directory;
 use file_management::File;
@@ -9,9 +11,16 @@ fn main() {
     let mut root : Directory = Directory::new_empty("../test");
     root.explore_hierarchy();
     
-    let prout = root.children_file[0].serialize();
-
-    let hiiii : File = File::deserialize(prout);
+    let prout = root.serialize();
+    let hiiii : Directory = Directory::deserialize(&prout).expect("problem :3");
     println!("{}", hiiii.path);
-    hiiii.print_file_content_as_str();
+    hiiii.print_arch();
+    for file in &hiiii.children_file{
+        file.print_file_content_as_str();
+    }
+
+    //let prout = root.children_file[0].serialize();
+    //let hiiii : File = File::deserialize(&prout).expect("problem :3");
+    //println!("{}", hiiii.path);
+    //hiiii.print_file_content_as_str();
 }
